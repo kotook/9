2a4b26758e9cd407cdc815250926ad0fe31cd150
kotook <85958338+kotook@users.noreply.github.com>
2022-06-03 17:24:38 +0300

    Update main.go

--- 2/main.go
+++ 2/main.go
@@ -1,14 +1,13 @@
-
 package main
 
 import "fmt"
 
 func main() {
-    msg := sayHello("Alice")
-    fmt.Println(msg)
+	msg := sayHello("Alice")
+	fmt.Println(msg)
 }
 
 func sayHello(name string) string {
-    return fmt.Sprintf("Hi %s", name)
+	// Измените это на "Hello %s" вместо "Hi %s".
+	return fmt.Sprintf("Hello %s", name)
 }
-var s int
